96bfa3005dd74755c7d1c87c02db6a77368d9945
samo <janezic.orion@gmail.com>
2023-01-12 17:11:35 +0100

    first Project

---
+++ test.php
@@ -0,0 +1,10 @@
+<form action="functions.php" method="get">
+  <input type="text" name="num01" placeholder="Number 1">
+  <select name="oper">
+    <label>Choose operation!</label>
+    <option value="add">Add</option>
+    <option value="sub">Substract</option>
+  </select>
+  <input type="text" name="num02" placeholder="Number 2">
+  <button type="submit">Calculate!</button>
+</form>
